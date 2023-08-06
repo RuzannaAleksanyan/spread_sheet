af490b25b5bbed547d00898ee0f2dc7c9988ce29
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-08-07 02:18:29 +0400

    Update and rename SpreadSheet.cpp to spread_sheet.cpp

--- spread_sheet.cpp
+++ spread_sheet.cpp
@@ -1,77 +1,4 @@
-#include <iostream>
-
-class Cell
-{
-	public:
-		virtual void setValue(const std::string& value) = 0;
-		virtual std::string getValue() const = 0;
-};
-
-class IntCell : public Cell
-{
-	private:
-		int m_value = 0;
-	public:
-		IntCell(int val) { m_value = val; }
-		void setValue(const std::string& value) { m_value = std::stoi(value);}
-		std::string getValue() const { return std::to_string(m_value); }
-};
-
-class DoubleCell : public Cell
-{
-	private:
-		double m_value = 0.0;
-	public:
-		DoubleCell(double val) { m_value = val; }
-		void setValue(const std::string& value) { m_value = std::stod(value);}
-		std::string getValue() const { return std::to_string(m_value); };
-};
-
-class StringCell : public Cell
-{
-	private:
-		std::string m_value;
-	public:
-		StringCell(std::string value) { m_value = value; }
-		void setValue(const std::string& value) { m_value = value;}
-		std::string getValue() const { return m_value; }
-};
-
-class SpreadSheet
-{
-	private:
-		Cell*** m_cells;
-		int m_row;
-		int m_col;
-	public:
-		SpreadSheet(int row, int col);
-		~SpreadSheet();
-
-		void setRow(int row) { m_row = row; }
-		void setCol(int col) { m_col = col; }
-		int getRow() { return m_row; }
-		int getCol() { return m_col; }
-
-		Cell*** getCells();		
-		void setCellAt(int row, int col, Cell* value);
-		void addRow(int count = 1);
-		void addColumn(int count = 1);
-		Cell* getCellAt(int row, int col);	
-};
-
-int main()
-{
-	SpreadSheet ob1(2, 3);
-    IntCell ob2(88);
-    DoubleCell ob3(1.5);
-	StringCell ob4("hello");
-    ob1.setCellAt(1, 1, &ob3);
-    ob1.setCellAt(0, 0, &ob2);
-	ob1.setCellAt(0, 1, &ob4);
-    std::cout << ob1.getCellAt(0, 0)->getValue() << " " << ob1.getCellAt(1, 1)->getValue();
-	std::cout << " " << ob1.getCellAt(0, 1)->getValue() << std::endl;
-	return 0;
-}
+#include "spreadSheet..hpp"
 
 Cell* SpreadSheet::getCellAt(int row, int col)
 {
@@ -180,4 +107,3 @@ SpreadSheet::~SpreadSheet()
     }
     delete []  m_cells;
 }
-
